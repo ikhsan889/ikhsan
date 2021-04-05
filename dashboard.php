a02bd20bb0e0a1389e261462ee20321b7db8a899
ikhsan889 <ikhsanmdv1@gmail.com>
2021-04-05 12:46:24 +0700

    projek ikhsan

---
+++ dashboard.php
@@ -0,0 +1,7 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  include ('tampilan/header.php');
+  include ('tampilan/sidebar.php');
+  include ('tampilan/footer.php');
+?>
+
